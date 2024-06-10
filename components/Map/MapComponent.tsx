f3df7b37ef2fcc574c5ba7a6cd1e2fa34841ea6a
이상범 <tkdqja975@naver.com>
2024-06-10 14:30:46 +0900

    자주 쓸 법한 지도 이벤트 정리

--- components/Map/MapComponent.tsx
+++ components/Map/MapComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   MapContainer,
   Marker,
@@ -21,15 +21,16 @@ import {
   alidadeSmoothDarkOSMProvider,
   baseOSMProvider,
 } from "@/utils/Map/Leaflet/osmProvider";
-import useGeoLocation from "@/hooks/Map/Leaflet/useGeoLocation";
+import useGeoLocation, {
+  GeoLocationProps,
+} from "@/hooks/Map/Leaflet/useGeoLocation";
 
 /**
  *
  * @참고자료 : https://leafletjs.com/reference.html
  */
 const MapComponent = () => {
-  const [isMount, setIsMount] = useState<boolean>(false);
-
+  const mapRef = useRef<any>(null);
   /**
    * @position : 창원 시청 중심좌표
    */
@@ -41,23 +42,31 @@ const MapComponent = () => {
   const markers = [
     {
       geoCode: { lat: 35.22783, lng: 128.679315 },
-      popup: "취급업체",
+      popup: "팝업 데이터 1",
     },
     {
       geoCode: { lat: 35.226360785943285, lng: 128.68222982315095 },
-      popup: "창원시청1",
+      popup: "팝업 데이터 2",
     },
     {
       geoCode: { lat: 35.2329, lng: 128.68222982315095 },
-      popup: "창원시청2",
+      popup: "팝업 데이터 3",
     },
     {
       geoCode: { lat: 35.23285, lng: 128.6722 },
-      popup: "창원시청3",
+      popup: "팝업 데이터 4",
     },
     {
       geoCode: { lat: 35.226360785943285, lng: 128.6722 },
-      popup: "창원시청4",
+      popup: "팝업 데이터 5",
+    },
+    {
+      geoCode: { lat: 35.24042520806185, lng: 128.64925766494147 },
+      popup: "사용자 위치 바운딩박스 (북동)",
+    },
+    {
+      geoCode: { lat: 35.240199191938146, lng: 128.64898093505852 },
+      popup: "사용자 위치 바운딩박스 (남서)",
     },
   ];
 
@@ -74,9 +83,6 @@ const MapComponent = () => {
    */
   const [clickLatLng, setClickLatLng] = useState<any | null>(null);
   const [centerLatLng, setCenterLatLng] = useState<any | null>(null);
-  useEffect(() => {
-    setIsMount(true);
-  }, []);
 
   /**
    * 클러스터 아이콘 생성
@@ -92,82 +98,165 @@ const MapComponent = () => {
   };
 
   const DrawingAndListingMarker = ({ markers }: any) => {
+    /**
+     * 이벤트 property 설명
+     * @type : 이벤트 유형 (click, popupopen 등)
+     * @target : 이벤트를 발생시킨 개체. 전파된 이벤트의 경우 이벤트를 발생시킨 전파 체인의 마지막 개체
+     * @sourceTarget : 이벤트를 발생시킨 개체. 원래 이벤트를 발생시킨 개체로, 전파되지 않은 이벤트의 경우 이는 target.
+     * @propagatedFrom : 전파된 이벤트의 경우 이벤트를 상위 이벤트에 전파한 마지막 개체
+     */
+
+    /**
+     * 이벤트 리스너
+     *
+     * @load : 지도가 초기화될 때(중심과 확대/축소가 처음으로 설정될 때) 실행
+     * @click : 사용자가 지도를 클릭(또는 탭)하면 실행
+     * @locationfound : 지리적 위치(locate method 사용) 로드가 성공적으로 수행되면 실행.
+     * @autopanstart : 팝업을 열 때 지도가 자동 이동을 시작하면 실행
+     * @movestart : 지도가 움직이기 시작할 때 실행
+     * @move : 지도가 움직이는 동안 실행
+     * @moveEnd : 지도가 움직인 후에 실행 (지도 중심이 변경되지 않으면 시작)
+     * @popupopen : 팝업 열릴 때 실행
+     * @popupclose : 팝업 닫힐 때 실행
+     */
     const map = useMapEvents({
-      // 지도가 초기화될 때(중심과 확대/축소가 처음으로 설정될 때) 시작됩니다.
       load(e) {
-        // console.log("지도초기화!!");
+        console.log("지도 초기화");
       },
 
+      // MouseEvent 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = MouseEvent 검색)
       click(e) {
+        /**
+         * @mapLocate : Geolocation API를 사용하여 사용자의 위치를 ​​찾으려고 시도한다.
+         *  - locationfound 성공 시 위치 데이터가 포함된 이벤트를 발생
+         *  - locationerror 실패 시 이벤트를 발생시키며 선택적으로 감지 정확도와 관련하여 지도 보기를 사용자 위치로 설정한다.
+         *
+         * @getBounds : 현재 지도 보기에 표시되는 지리적 경계를 반환 (북동 / 남서)
+         *  - mapLocate.getBounds()
+         *
+         * @getCenter : 지도 중심 반환
+         *  - mapLocate.getCenter()
+         *
+         * @getZoom @getMinZoom @getMaxZoom : 현재, 최소, 최대 zoomLevel 반환
+         *  - mapLocate.getZoom(), mapLocate.getMinZoom(), mapLocate.getMaxZoom()
+         */
         const mapLocate = map.locate();
-        // console.log("바운더리 (북동 / 남서): ", clickEvent.getBounds());
-        // console.log("현재 지도의 중심: ", clickEvent.getCenter());
-        // console.log(
-        //   "현재, 최소, 최대 zoom : ",
-        //   clickEvent.getZoom(),
-        //   clickEvent.getMinZoom(),
-        //   clickEvent.getMaxZoom()
-        // );
-        //console.log("클릭된 위치 : ", e.latlng);
-
-        setClickLatLng(e.latlng);
         setCenterLatLng(mapLocate.getCenter());
+
+        /**
+         * @e : MouseEvent
+         *
+         * @latlng : MouseEvent에 속하는 객체이며, 이벤트가 발생한 지리적 지점 (위도/경도값)
+         *  - e.latlng
+         *
+         * @containerPoint : MouseEvent에 속하는 객체이며, 지도 레이어를 기준으로 마우스 이벤트가 발생한 지점의 픽셀 좌표
+         *  - e.containerPoint
+         *
+         * @containerPoint : MouseEvent에 속하는 객체이며, 지도 컨테이너를 기준으로 마우스 이벤트가 발생한 지점의 픽셀 좌표
+         *  - e.containerPoint
+         */
+        setClickLatLng(e.latlng);
       },
 
-      // 지리적 위치( locate메서드 사용)가 성공적으로 수행되면 실행됩니다.
+      // LocationEvent 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = LocationEvent 검색)
       locationfound(e) {
-        // console.log(
-        //   "사용자 위치를 확인하는 locationFound 이벤트 발생 : ",
-        //   e,
-        //   map.getZoom()
-        // );
-        // 부드러운 팬-줌 애니메이션을 수행하는 지도 보기(지리적 중심 및 확대/축소)를 설정합니다.
-        // map.flyTo(clickLatLng, map.getZoom());
-        // map.flyTo(e.latlng, map.getZoom());
+        /**
+         * @e : LocationEvent
+         *
+         * @latlng : LocationEvent에 속하는 객체이며, 이벤트가 발생한 지리적 지점 (위도/경도값)
+         *  - e.latlng
+         *
+         * @bounds : LocationEvent에 속하는 객체이며, 사용자가 위치한 지역의 바운딩 박스 반환
+         *  - e.bounds
+         *
+         * @accuracy : 미터 단위의 위치 정확도
+         *  - e.accuracy
+         */
+        const clickEventLatLng = e.latlng;
+
+        /**
+         * @flyTo : 지정한 위치로 자연스럽게 이동 (이동할 좌표 위치)
+         *  - map.flyTo(clickLatLng, map.getZoom());
+         */
       },
 
-      autopanstart(e) {
-        // console.log("팝업을 열 때 지도가 자동 이동을 시작하면 시작됩니다.", e);
-      },
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      autopanstart(e) {},
 
-      // 지도 보기가 변경되기 시작할때
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
       movestart(e) {
-        // console.log("움직이기 시작!!");
+        const moveEvent = map.locate();
+        const currentMapCenter = moveEvent.getCenter();
+        console.log("moveStart ", currentMapCenter);
       },
 
-      move(e) {
-        // 이거하면 애니메이션이 뚝뚝 끊겨서 보임
-        // const moveEvent = map.locate();
-        // const currentMapCenter = moveEvent.getCenter();
-        // setCenterLatLng(currentMapCenter);
-      },
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      // 잦은 호출 때문에, 성능이 떨어질 수 있음
+      move(e) {},
 
-      // 지도 중심이 변경되지 않으면 시작됩니다(예: 사용자가 지도 드래그를 중지했거나 중심에서 벗어나 확대/축소한 후).
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
       moveend(e) {
         const moveEvent = map.locate();
         const currentMapCenter = moveEvent.getCenter();
-        // console.log("움직이기 종료!!", currentMapCenter);
-
-        // setTimeout(() => {
-        //   // 클러스터 애니메이션을 다 보고 움직이고 싶으면 0.5초 정도 간격을 주고 실행
-        // setCenterLatLng(currentMapCenter);
-        // }, 500);
+        console.log("moveEnd ", currentMapCenter);
       },
 
-      // 팝업 열릴 때 실행
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      zoomstart(e) {
+        const moveEvent = map.locate();
+        const currentZoom = moveEvent.getZoom();
+        console.log("zoomStart ", currentZoom);
+      },
+
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      zoom(e) {},
+
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      zoomend(e) {
+        const moveEvent = map.locate();
+        const currentZoom = moveEvent.getZoom();
+        console.log("zoomEnd ", currentZoom);
+      },
+
+      // PopupEvent 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = PopupEvent 검색)
       popupopen(e) {
-        // 클릭한 팝업의 좌표
+        /**
+         * @e : PopupEvent
+         *
+         * @getLatLng : 팝업의 latLng이라기보다는 '마커'의 latLng을 가져오는 방법이다.
+         *  - markerData에 있는 latlng 값이다.
+         *  - 마커 클릭 시, 지도 중앙으로 오게 해야한다면 이걸 쓰면 된다.
+         *  - e.popup.getLatLng()
+         */
+
         const flyTargetLatLng = e.popup.getLatLng();
         if (flyTargetLatLng) {
-          // 이거 클릭한건 markerData에 있는 latlng와 동일
-          // console.log("flyTargetLatLng", flyTargetLatLng);
-          // map.flyTo(flyTargetLatLng, map.getZoom());
+          //  map.flyTo(flyTargetLatLng, map.getZoom());
         }
       },
 
-      // 팝업 닫힐 때 실행
-      popupclose(e) {
-        // console.log("닫힘!!");
+      // Event 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = Event 검색)
+      popupclose(e) {},
+
+      // LayerEvent 가짐
+      // https://leafletjs.com/reference.html#locationevent 에서 (CTRL+F = LayerEvent 검색)
+      layeradd(e) {
+        /**
+         * @getPopup : 이 레이어에 바인딩된 팝업을 반환
+         *  - e.layer.getPopup()
+         */
+        const popup = e.layer.getPopup();
       },
     });
 
@@ -195,74 +284,115 @@ const MapComponent = () => {
     );
   };
 
+  /**
+   * 사용자 위치 정보를 불러오는 useGeoLocation hooks 사용
+   */
+  const [loadLocation, setLoadLocation] = useState<GeoLocationProps | null>(
+    null
+  );
   const location = useGeoLocation();
-  console.log("location is ", location);
+  useEffect(() => {
+    if (location) {
+      setLoadLocation(location);
+    }
+  }, [location]);
 
   return (
     <>
-      {isMount ? (
-        <div className="map_wrap">
-          <section className="section">
-            <div className={styles.section_top}>
-              {/* 현재 클릭된 점의 좌표 */}
-              <div className={styles.event_desc}>
-                <span className={styles.desc_title}>지도 클릭좌표</span>
-                <span className={styles.desc_value}>
-                  {clickLatLng
-                    ? `: ${clickLatLng.lat} : ${clickLatLng.lng}`
-                    : ": 클릭되지 않음"}
-                </span>
-              </div>
-
-              {/* 현재 중심좌표 */}
-              <div className={styles.event_desc}>
-                <span className={styles.desc_title}>지도 중심좌표</span>
-                <span className={styles.desc_value}>
-                  {centerLatLng
-                    ? `: 위도 : ${centerLatLng.lat} : ${centerLatLng.lng}`
-                    : ": 중심좌표가 설정되지 않음"}
-                </span>
-              </div>
+      <div className="map_wrap">
+        <section className="section">
+          <div className={styles.section_top}>
+            {/* 현재 클릭된 점의 좌표 */}
+            <div className={styles.event_desc}>
+              <span className={styles.desc_title}>지도 클릭좌표</span>
+              <span className={styles.desc_value}>
+                {clickLatLng
+                  ? `: ${clickLatLng.lat} : ${clickLatLng.lng}`
+                  : ": 클릭되지 않음"}
+              </span>
             </div>
-            <div className={styles.section_bottom}>
-              <div className={styles.event_btn_box}></div>
+
+            {/* 현재 중심좌표 */}
+            <div className={styles.event_desc}>
+              <span className={styles.desc_title}>지도 중심좌표</span>
+              <span className={styles.desc_value}>
+                {centerLatLng
+                  ? `: 위도 : ${centerLatLng.lat} : ${centerLatLng.lng}`
+                  : ": 중심좌표가 설정되지 않음"}
+              </span>
             </div>
-          </section>
+          </div>
+          <div className={styles.section_bottom}>
+            <div className={styles.event_btn_box}>
+              <button
+                onClick={() => {
+                  if (mapRef && mapRef.current) {
+                    const mapObj = mapRef.current.locate();
 
-          {/* 뭔가를 추가할 때마다 저작권 창에 하나씩 뭔가 추가가됨 */}
-          <MapContainer
-            // https://leafletjs.com/reference.html#map-methods-for-getting-map-state
-            center={position}
-            zoom={DEFAULT_LEAFLET_ZOOM_LEVEL}
-            minZoom={DEFAULT_LEAFLET_ZOOM_MIN_ZOOM}
-            maxZoom={DEFAULT_LEAFLET_ZOOM_MAX_ZOOM}
-            zoomControl={true}
-            scrollWheelZoom={true}
-            closePopupOnClick={true}
-            // zoomSnap={2}
-          >
-            <TileLayer
-              attribution={baseOSMProvider.maptiler.atttribution}
-              url={baseOSMProvider.maptiler.url}
-            />
+                    mapObj.flyTo(loadLocation?.coordinates, mapObj.getZoom());
+                    // console.log(
+                    //   "mapObj is ",
+                    //   mapObj.getCenter(),
+                    //   mapObj.getZoom()
+                    // );
+                  }
+                }}
+              >
+                현재 위치로 이동
+              </button>
+            </div>
+          </div>
+        </section>
 
-            {/* leaflet skin이라고 검색하고 끼워넣으면 됨 */}
-            {/* <TileLayer
+        {/* 뭔가를 추가할 때마다 저작권 창에 하나씩 뭔가 추가가됨 */}
+        <MapContainer
+          // https://leafletjs.com/reference.html#map-methods-for-getting-map-state
+          center={position}
+          zoom={DEFAULT_LEAFLET_ZOOM_LEVEL}
+          minZoom={DEFAULT_LEAFLET_ZOOM_MIN_ZOOM}
+          maxZoom={DEFAULT_LEAFLET_ZOOM_MAX_ZOOM}
+          zoomControl={true}
+          scrollWheelZoom={true}
+          closePopupOnClick={true}
+          ref={mapRef}
+          // zoomSnap={2}
+        >
+          <TileLayer
+            attribution={baseOSMProvider.maptiler.atttribution}
+            url={baseOSMProvider.maptiler.url}
+          />
+
+          {/* leaflet skin이라고 검색하고 끼워넣으면 됨 */}
+          {/* <TileLayer
               attribution={alidadeSmoothDarkOSMProvider.maptiler.atttribution}
               url={alidadeSmoothDarkOSMProvider.maptiler.url}
             /> */}
-            {/* 특정 줌 이하가 되면 몇건이 모여있는지 표시함 */}
-            <MarkerClusterGroup
-              chunkedLoading
-              iconCreateFunction={createCustomClusterIcon}
+          {/* 특정 줌 이하가 되면 몇건이 모여있는지 표시함 */}
+          <MarkerClusterGroup
+            chunkedLoading
+            iconCreateFunction={createCustomClusterIcon}
+          >
+            <DrawingAndListingMarker markers={markers} />;
+          </MarkerClusterGroup>
+
+          {loadLocation && loadLocation.coordinates ? (
+            <Marker
+              position={loadLocation!.coordinates}
+              icon={customIcon}
+              title={"현재 위치"}
             >
-              <DrawingAndListingMarker markers={markers} />;
-            </MarkerClusterGroup>
-          </MapContainer>
-        </div>
-      ) : (
-        <></>
-      )}
+              <Popup>
+                <div className="popup_wrap">
+                  <div className={"title"}>현재위치 표시입니다.</div>
+                  <div className="desc">안녕하세요.</div>
+                </div>
+              </Popup>
+            </Marker>
+          ) : (
+            <></>
+          )}
+        </MapContainer>
+      </div>
     </>
   );
 };
--- hooks/Map/Leaflet/useGeoLocation.ts
+++ hooks/Map/Leaflet/useGeoLocation.ts
@@ -1,40 +1,73 @@
 "use client";
 import { useEffect, useLayoutEffect, useState } from "react";
 
+export interface GeoLocationProps {
+  /**
+   * @loaded : 데이터 로드여부
+   */
+  loaded: boolean;
+  /**
+   * @coordinates : 사용자 좌표
+   */
+  coordinates: {
+    lat: number;
+    lng: number;
+  } | null;
+  /**
+   * @error : error Msg
+   */
+  error?: {
+    code: number;
+    message: string;
+  };
+}
 /**
  * @useGeoLocation : 현재 위치를 반환해주는 함수
  * @returns location
  */
 const useGeoLocation = () => {
-  const [location, setLocation] = useState({
+  const [location, setLocation] = useState<GeoLocationProps>({
     loaded: false,
-    coordinates: { lat: "", lng: "'" },
+    coordinates: null,
   });
 
+  /**
+   * @onSuccess : 사용자 위치 로드 성공 시 error는 undefined, coordinates 값 추가
+   */
   const onSuccess = (location: GeolocationPosition) => {
-    console.log("onSuccess location is ", location);
     setLocation({
       loaded: true,
       coordinates: {
         lat: location.coords.latitude,
         lng: location.coords.longitude,
       },
+      error: undefined,
     });
   };
 
-  const onError = (error) => {
+  /**
+   * @onError : 사용자 위치 로드 실패 시 error 추가, coordinates 값 null
+   */
+  const onError = (error: GeolocationPositionError) => {
     setLocation({
       loaded: false,
-      error,
+      coordinates: null,
+      error: {
+        code: error.code,
+        message: error.message,
+      },
     });
   };
 
+  /**
+   * @useLayoutEffect : 사용자 위치 로드 시도
+   */
   useLayoutEffect(() => {
     if (!("geolocation" in navigator)) {
       setLocation((prev) => {
         return {
-          ...prev,
-          loaded: true,
+          loaded: false,
+          coordinates: null,
           error: {
             code: 500,
             message:
